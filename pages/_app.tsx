17c7fbfe458417a39a28ef0aedf1bc4ab220714e
Brian Ding <brian_ding@outlook.com>
2022-10-30 17:23:23 +0800

    add fluent ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  createDOMRenderer,
+  FluentProvider,
+  GriffelRenderer,
+  SSRProvider,
+  RendererProvider,
+  webLightTheme,
+} from '@fluentui/react-components';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type EnhancedAppProps = AppProps & { renderer?: GriffelRenderer };
+
+function MyApp({ Component, pageProps, renderer }: EnhancedAppProps) {
+  return (
+    // 👇 Accepts a renderer from <Document /> or creates a default one
+    //    Also triggers rehydration a client
+    <RendererProvider renderer={renderer || createDOMRenderer()}>
+      <SSRProvider>
+        <FluentProvider theme={webLightTheme}>
+          <Component {...pageProps} />
+        </FluentProvider>
+      </SSRProvider>
+    </RendererProvider>
+  );
 }
+
+export default MyApp;
