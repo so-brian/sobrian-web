560b50254fb838b2692918e74ca1abb2c01263ac
Brian Ding <brian_ding@outlook.com>
2025-04-06 20:19:04 +0800

    ft: revert next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
